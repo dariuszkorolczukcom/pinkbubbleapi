0769df3a95c4424fbaf209011f037f1cbad009d2
dariuszkorolczukcom <korolczuk86@gmail.com>
2020-07-04 13:01:45 +0100

    logging db

--- database/database.go
+++ database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jinzhu/gorm"
@@ -9,15 +10,18 @@ import (
 var Conn *gorm.DB
 
 func InitDB() (Conn *gorm.DB) {
+	fmt.Println("initialising DB")
 	dbuser := os.Getenv("DBUSER")
 	dbpassword := os.Getenv("DBPASSWORD")
 	dbname := os.Getenv("DBNAME")
 	dbhost := os.Getenv("DBHOST")
 	dbport := os.Getenv("DBPORT")
+	fmt.Println("mysql", dbuser+":"+dbpassword+"@("+dbhost+":"+dbport+")/"+dbname+"?charset=utf8&parseTime=true")
 	Conn, err := gorm.Open("mysql", dbuser+":"+dbpassword+"@("+dbhost+":"+dbport+")/"+dbname+"?charset=utf8&parseTime=true")
 	if err != nil {
 		panic("failed to connect database")
 	}
+	fmt.Println("DB initiallised")
 	Conn.LogMode(true)
 	return
 }
